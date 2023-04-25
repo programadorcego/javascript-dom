5ef3aacd9ec60dae483a244a288c93cc9c904234
Programador Cego <programadorcego@gmail.com>
2023-04-25 07:13:35 -0300

    Aula 05: getElementById

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
-/*
-O dom (Document Object Model) em JavaScript é uma representação da estrutura em árvore de um documento HTML.
-Ele permite que o JavaScript acesse e manipule elementos em uma página web de forma dinâmica, o que significa que o conteúdo pode ser adicionado, removido ou modificado sem precisar recarregar a página inteira.
-O dom é formado por nós (nodes), que podem ser elementos HTML, atributos, texto, comentários, entre outros.
-Cada nó possui propriedades e métodos que podem ser acessados pelo JavaScript para alterar o conteúdo e o comportamento da página.
-O dom é uma parte fundamental da programação web em JavaScript e permite a criação de páginas dinâmicas e interativas.
-*/
+// O método getElementById é utilizado para selecionar um elemento HTML específico com base no atributo id.
 
-let element = document;
+let texto = document.getElementById('text');
+//console.log(texto);
 
-// Retornando os elementos do <body</body>
-//console.log(element.body);
-
-// Retornando os elementos do <head</head>
-//console.log(element.head);
-
-// Retornando os elementos do <html</html>
-console.log(element.documentElement);
+//texto.innerText = "Meu Texto Alterado";
+texto.innerHTML = "<strong>Meu Texto Alterado</strong>";
