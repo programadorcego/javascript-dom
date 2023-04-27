8ad0a81b12e7cbc14bfb659a0e4cfeaf32f86357
Programador Cego <programadorcego@gmail.com>
2023-04-27 07:31:43 -0300

    Aula 12: getComputedStyle

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-//document.body.style.backgroundColor = '#000';
-//document.body.style.color = '#fff';
+/*
+O método getComputedStyle() obtém as propriedades CSS computadas e os valores de um elemento HTML.
+O método getComputedStyle() retorna um objeto CSSStyleDeclaration.
+O estilo calculado é o estilo usado no elemento depois que todas as fontes de estilo foram aplicadas.
+Fontes de estilo: folhas de estilo externas e internas, estilos herdados e estilos padrão do navegador.
+*/
 
-console.log(document.body.style.backgroundColor);
+let texto = document.querySelector('.text');
+let cssStyle = getComputedStyle(texto);
+
+texto.style.fontSize = `${parseInt(cssStyle.fontSize) * 2}px`;
