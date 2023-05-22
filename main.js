55d0d196c88a5dcd221b68a46a5ba85c92d8929e
Programador Cego <programadorcego@gmail.com>
2023-05-22 17:00:33 -0300

    Aula 17: Exercício 1

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-document.querySelector('.form').addEventListener('submit', e => {
-	e.preventDefault();
+/*
+A partir do HTML existente, receber um número como valor de entrada.
+Calcular e informar seus vizinhos ou seja,
+o antecessor e o sucessor.
+*/
+
+document.querySelector('.btn').addEventListener('click', () => {
+	const numero = document.querySelector('#numero');
 	
-	let nome = document.querySelector('#nome');
-	
-	document.querySelector('.texto').textContent = `Meu nome é ${nome.value}`;
+	document.querySelector('.antecessor').textContent = Number(numero.value) - 1;
+	document.querySelector('.sucessor').textContent = Number(numero.value) + 1;
 });
