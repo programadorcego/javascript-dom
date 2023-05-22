2ed4a12dab3773efd1a3a4e6bb958c168e090231
Programador Cego <programadorcego@gmail.com>
2023-05-22 16:59:21 -0300

    Aula 16: Recuperando dados de um formulário

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-/*
-O método getComputedStyle() obtém as propriedades CSS computadas e os valores de um elemento HTML.
-O método getComputedStyle() retorna um objeto CSSStyleDeclaration.
-O estilo calculado é o estilo usado no elemento depois que todas as fontes de estilo foram aplicadas.
-Fontes de estilo: folhas de estilo externas e internas, estilos herdados e estilos padrão do navegador.
-*/
-
-let texto = document.querySelector('.text');
-let cssStyle = getComputedStyle(texto);
-
-texto.style.fontSize = `${parseInt(cssStyle.fontSize) * 2}px`;
+document.querySelector('.form').addEventListener('submit', e => {
+	e.preventDefault();
+	
+	let nome = document.querySelector('#nome');
+	
+	document.querySelector('.texto').textContent = `Meu nome é ${nome.value}`;
+});
