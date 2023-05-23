c0a7c439921fc87919f6a334aed80f8281d47ffb
Programador Cego <programadorcego@gmail.com>
2023-05-23 11:36:37 -0300

    Aula 19: Exercício #3

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 /*
-A partir do HTML existente, apresentar um valor inicial igual a 0 e definir
-    funcionalidades nos botões de decremento e incremento. Ao clicar em cada
-    um dos botões, o utilizador irá aumentar ou diminuir o valor em uma unidade.
-O valor nunca poderá ser inferior a -10 ou superior a 10.
+A partir do HTML existente, ler o valor total de uma conta de uma pizzaria 
+e quantos clientes vão pagá-la.
+Calcule e informe o valor a ser pago por cliente.
 */
 
-let valor = 0;
-
-function incrementar(){
-	if(valor < 10) document.querySelector('.valor').innerText = ++valor;
-}
-
-function decrementar(){
-	if(valor > -10) document.querySelector('.valor').innerText = --valor;
-}
-
-document.querySelector('.incremento').addEventListener('click', incrementar);
-document.querySelector('.decremento').addEventListener('click', decrementar);
+document.querySelector('.btn').addEventListener('click', () => {
+	const total = document.querySelector('#total');
+	const clientes = document.querySelector('#clientes');
+	let total_cliente = parseFloat(total.value.replace(',', '.')) / parseInt(clientes.value);
+	
+	document.querySelector('.resultado').innerHTML = `
+		<p>Valor da Conta: R$ ${parseFloat(total.value.replace(',', '.')).toFixed(2).replace('.', ',')}</p>
+		<p>Número de Clientes: ${clientes.value}</p>
+		<p>Valor por Cliente: R$ ${total_cliente.toFixed(2).replace('.', ',')}</p>
+	`;
+});
