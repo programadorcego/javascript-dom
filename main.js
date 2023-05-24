87ea7432bbe870f7f78f5000eb58fc4538ddf7c2
Programador Cego <programadorcego@gmail.com>
2023-05-24 18:44:05 -0300

    Aula 14: Evnt Handlers

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
-/*
-No JavaScript também é possível manipularmos as classes CSS de um elemento HTML.
-Para isso utilizamos as propriedades:
-1. className: retorna ou define a lista de classes CSS de um elemento HTML como uma string.
-2. classList: retorna um objeto que representa a lista de classes CSS de um elemento HTML. Podemos usar métodos como `add()`, `remove()`, `toggle()`, `contains()` e `replace()` para adicionar, remover, alternar, verificar e substituir classes específicas.
-*/
-
-let element = document.querySelector('.container');
-//console.log(element.className);
-
-//console.log(element.classList);
-
-element.classList.remove('container-dark');
-element.classList.add('container-light');
-console.log(element.classList.contains('container-light'));
-element.classList.toggle('invisivel');
+function funcao(){
+	document.body.style.background = '#000';
+	document.body.style.color = '#fff';
+}
