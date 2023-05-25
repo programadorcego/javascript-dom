bfa8741975572f0935fe637d9fabdb8525493f9a
Programador Cego <programadorcego@gmail.com>
2023-05-25 18:56:59 -0300

    Aula 15: addEventListener

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-function funcao(){
-	document.body.style.background = '#000';
-	document.body.style.color = '#fff';
-}
+/*
+O método `addEventListener()` é usado para anexar um event handler a um elemento HTML, a fim de capturar e lidar com eventos específicos,
+como cliques do mouse, pressionamentos de tecla, submissões de formulários, etc.
+*/
+
+document.querySelector('button').addEventListener('click', function(){
+	//alert('O botão foi clicado');
+	
+	document.querySelector('p').innerText = "O texto foi alterado";
+});
